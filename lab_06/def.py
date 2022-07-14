dc6578c5b778bed81132495892b2edf419f67fe1
Shimshir Emirdzan IU7-23B <shimshireao@student.bmstu.ru>
2022-07-14 22:30:25 +0300

    fix 1-6 labs

--- lab_06/def.py
+++ lab_06/def.py
@@ -2,7 +2,6 @@ a = list(map(int, input('Введите массив a: ').split()))
 b = list(map(int, input('Введите массив b: ').split()))
 c = list(map(int, input('Введите массив c: ').split()))
 
-
 for i in b:
 	if i in c:
 		b.remove(i)
@@ -11,12 +10,12 @@ for i in c:
 	if i in a:
 		a.remove(i)
 		
-
 for i in b:
 	if i in a:
 		a.remove(i)
 
 if len(a) >= 2:
 	a[0], a[-1] = a[-1], a[0]
+
 print('Элементы, которые есть в a, но их нет в b и c. \
 Первый и полследний элементы меняются местами при необходимости:', a)
